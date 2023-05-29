434bba5f30b604c9160a6feaf41cf87f9d38b24a
hasan-lodhi <133870736+hasan-lodhi@users.noreply.github.com>
2023-05-29 16:21:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var a = document.getElementById("hasan");
+
+function circle() {
+  a.className = "circle";
+ 
+}
+function triangle() {
+  a.className = "triangle";
+}
+function square() {
+  a.className = "square";
+}
+
+
+
